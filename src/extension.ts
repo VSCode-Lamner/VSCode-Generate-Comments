13c92ead15ffce3e2732c4cf6847bd688e8afb38
kaminskg <gkamin6@gmail.com>
2021-06-14 08:32:57 -0700

    Add ability to insert comments and close preview

--- src/extension.ts
+++ src/extension.ts
@@ -1,59 +1,77 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "vsgencomments" is now active!');
-  
-  let disposable = vscode.commands.registerCommand("vsgencomments.insertComment", () => {
-    // let content: string = "// This is a comment\n// that is 'generated' and\n// pulled from the server\n// ^_^\n\nGENIOUS";
+console.log('Congratulations, your extension "vsgencomments" is now active!');
+vscode.window.showInformationMessage("Extension is running");
+
+let disposable = vscode.commands.registerTextEditorCommand("vsgencomments.insertComment", () => {
 
     // Get instance of edtior
-    const edtior  = vscode.window.activeTextEditor;
-    if (!edtior) {return;}
+    const editor  = vscode.window.activeTextEditor;
+    if (!editor) {return;}
+
+    // Select the text in the active editor
+    let selection = editor.selection;
     
-    let content: string = "// Guy and Rocco Make and amazing team!!!";
-    // Get language
-    let lang: string = edtior.document.languageId;
+    // Get the starting line above the selected text
+    let startLine = selection.start.line;
     
-    // debug message - to remove
-    console.log(lang);
+    // Select the text
+    // TODO: Use a POST request, to send the code to receive the comment
+    let selectedText = editor.document.getText(selection);
+    
+    // TODO: Use a GET request to receive this comment
+    let commentToInsert: string = "// Guy and Rocco Make and amazing team!!!";
+    // let commentToInsert: string = 
+    //     "/* Guy and Rocco \n * Make an \n" + 
+    //     " * amazing team!!!\n */";
+    
+    // Get language the user is using
+    let lang: string = editor.document.languageId;
 
     // perform async operation to utilize textDocument 'thenable' promise
     (async () => {
-      
-      // debug message - to remove
-      console.log("Trying to show the window");
-      
-      // Open document with the function with comment and language of the editor
-      const previewDoc = await vscode.workspace.openTextDocument(
-        {language: lang, content: content}
-      );
-      // Put the editor side by side 
-      vscode.window.showTextDocument(previewDoc, vscode.ViewColumn.Beside, true);
-  
-      //  debug message to remove
-      console.log("guess we already did that");
+        // Open document with the function with comment and language of the editor
+        const previewDoc = await vscode.workspace.openTextDocument(
+            {language: lang, content: (commentToInsert + "\n" + selectedText)}
+        );
 
-      let userOptions: string[] = ["Yes: Close editor", "No: Leave open", "No: Close editor"];
-      const selection = await vscode.window.showInformationMessage("Here is your code with a comment!", ...userOptions).then(selection => {
-        if (selection === userOptions[0]) {
-        // Insert comment 
-        vscode.window.showInformationMessage("Cool");
-      } else if (selection === userOptions[1]) {
-        vscode.window.showInformationMessage("Alright, we await your commands");
-      } else {
-        // Close window?
-        vscode.window.showInformationMessage("Our apologies, sorry to bother you");
-      }
-      });
+        // Put the editor side by side 
+        vscode.window.showTextDocument(previewDoc, vscode.ViewColumn.Beside, false);
 
+        let userOptions: string[] = ["Yes: Close editor", "No: Leave open", "No: Close editor"];
+        const selection = await vscode.window.showInformationMessage(
+            "Here is your code with a comment!", 
+            ...userOptions).then(selection => 
+        {
+            // Insert the comment 
+            if (selection === userOptions[0]) {
+                commentToInsert += "\n";
 
-      
-    })();
+                let positionToInsert: vscode.Position = new vscode.Position(startLine, 0); 
+                editor.edit((editBuilder: vscode.TextEditorEdit) => {
+                    editBuilder.insert(positionToInsert, commentToInsert);
+                });
+
+                (async () => {
+                    await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+                })();
+                
+                vscode.window.showInformationMessage("Cool");
+
+            } else if (selection === userOptions[1]) {
+                vscode.window.showInformationMessage("Sounds good, no worries");
+            } else {
+                (async () => {
+                    await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+                })();
+                
+                vscode.window.showInformationMessage("Our apologies, sorry to bother you");
+            }
+        });
     
-      vscode.window.showInformationMessage("Extension is running");
-    }
-  );
-  
-  context.subscriptions.push(disposable);
+    })();
+});
+context.subscriptions.push(disposable);
 }
 export function deactivate() {}
